95fbdeee61fe2c22a6488e9aac830dc3dbeb7537
diegovalverde <diego.valverde.g@gmail.com>
2015-12-20 12:17:01 -0600

    new: dev: Added simple script to check log file values @wip

---
+++ script/check_bios_log.py
@@ -0,0 +1,50 @@
+import re
+from sys import argv
+
+reg_names = ['SP', 'B', 'C', 'D', 'E', 'H', 'L', 'A', 'Flags']
+#                PC      SP    B    C    D    E     H    L   A    Flags
+#                 0x0000 LD SP, $0xFFFE
+bios_checker = {'00000003': ['fffe', '*', '*', '*', '*', '*', '*', '*', '*'],
+                # 0x0003 XOR A
+                '00000004': ['*', '*', '*', '*', '*', '*', '*', '00', '*'],
+                # 0x000C  LD HL, $0xFF26 # load 0xFF26 to HL
+                '0000000f': ['*', '*', '*', '*', '*', 'ff', '26', '*', '*'],
+                # 0x000F  LD C, $0x11 # load 0x11 to C
+                '00000011': ['*', '*', '11', '*', '*', '*', '*', '*', '*'],
+                # 0x0011  LD A, $0x80 # load 0x80 to A
+                '00000013': ['*', '*', '*', '*', '*', '*', '*', '80', '*'],
+                # 0x0013  LD (HL-), A # load A [HL], HL--
+                '00000015': ['*', '*', '*', '*', '*', 'ff', '25', '*', '*'],
+                # 0x0015  INC C # increment C register
+                '00000016': ['*', '*', '12', '*', '*', '*', '*', '*', '*'],
+                # 0x0016  LD A, $0xF3 # load 0xF3 to A
+                '00000018': ['*', '*', '*', '*', '*', '*', '*', 'f3', '*'],
+                # 0x001A  LD A, $0x77 # load 0x77 to A
+                '0000001c': ['*', '*', '*', '*', '*', '*', '*', '77', '*'],
+                # 0x001D LD A, $0xFC # A represents the color number mappings
+                '0000001f': ['*', '*', '*', '*', '*', '*', '*', 'fc', '*']
+                }
+
+
+script, filename = argv
+
+with open(filename, "r") as file:
+    for line in file:
+        if (not re.match("^\[regs\]", line)):
+            continue
+
+        regs = re.split("\s+", line)
+        pc = regs[1]
+        regs = regs[2:]
+
+        if (pc not in bios_checker):
+            continue
+
+        print(line)
+        for reg, value, name in zip(regs, bios_checker[pc], reg_names):
+            if (value == '*'):
+                continue
+
+            print(("pc {0}. {1} {2} found {3}").format(pc, name, value, reg))
+            if (value != reg):
+                raise Exception(("Register mismatch\n"))
