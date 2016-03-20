28b166827773023ddcade981b3d68bd22afe6f47
Marco Montero <marco_montero1991@hotmail.com>
2016-03-20 16:50:40 -0600

    added comments to gpu.v and gpu_ucode for better understanding and removed unnecesary commented lines

--- rtl/gpu_ucode_rom.v
+++ rtl/gpu_ucode_rom.v
@@ -35,6 +35,8 @@ begin
   case ( iAddr )
 
   /*
+/////////////PSEUDO CODE FOR BETTER UNDERSTANDING////////////////////
+
       state = SCANLINE_VRAM_READ
       cur_tile = 0
 1:
@@ -70,54 +72,42 @@ begin
 
 
   */
+///////////////////////////////////////////////////////////////////////////
 
-
-  0 : oUop = {  `gnop,  `gnull, `gnull, `gnull };
-  1 : oUop = {  `gwrl,  `state, `SCANLINE_VRAM_READ };
-  //2 : oUop = {  `gwrl,  `cur_tile, 10'd0}; //TODO replace by { `gwrr, `cur_tile, `scy_shl_5__plus_scx }
-  2 : oUop =  { `gwrr, `cur_tile, `scy_shl_5__plus_scx, `gnull };
-//1
-  //3 : oUop = {  `gwrl,  `tile_row, 10'd0 };
-  3: oUop = { `gwrr, `tile_row, `scy_tile_row_offset, `gnull};
-//2
-  4 : oUop = {  `gwrl,  `r2, 10'd32 };
-//3
-  5: oUop = {  `gadd,  `vmem_addr, `bgmoffset, `cur_tile };
-  6: oUop = {  `grvmem, `gnull, `gnull, `gnull };
-  7: oUop = {  `gwrr, `r3, `vmem_addr, `gnull };
-  8: oUop = {  `gadd,  `r1,        `vmem_data_shl_4, `tile_row };
-  9: oUop = {  `gadd,  `vmem_addr, `r1, `bgtoffset };
-  10: oUop = {  `grvmem, `gnull, `gnull, `gnull };
-
-
-  11: oUop = {  `gwrr,   `bh, `vmem_data, `gnull };
-  12: oUop = {  `gaddl,  `vmem_addr, 10'd1  };
-  13: oUop = {  `grvmem, `gnull, `gnull, `gnull };
-  14: oUop = {  `gwrr,   `bl, `vmem_data, `gnull };
-  15: oUop = {  `gwbg,   `gnull,`gnull, `gnull};
-
-
-  16: oUop = { `gsubl, `r1, 10'd8191};
+//defines code for getting the first tile and applying color pallete to it
+  0: oUop = { `gnop,  `gnull, `gnull, `gnull }; //always has to be a nop because of pc definition
+  1: oUop = { `gwrl,  `state, `SCANLINE_VRAM_READ };// writes a literal to register state for cpu to know which state the GPU currently is
+  2: oUop = { `gwrr, `cur_tile, `scy_shl_5__plus_scx, `gnull };//which tile am I on, (oScy >>3) <<5 + oScx)
+  3: oUop = { `gwrr, `tile_row, `scy_tile_row_offset, `gnull}; // which tile row am I on,
+  4: oUop = { `gwrl,  `r2, 10'd32 };//assign a literal to r2, 32 tiles
+  5: oUop = { `gadd,  `vmem_addr, `bgmoffset, `cur_tile };// adds current tile with bg offset to get vmem adress to know where to read
+  6: oUop = { `grvmem, `gnull, `gnull, `gnull };//reads vmem and stores in vmem_data 
+  7: oUop = { `gwrr, `r3, `vmem_addr, `gnull };// store vmem adress in r3
+  8: oUop = { `gadd,  `r1,`vmem_data_shl_4, `tile_row };//add tile row with vmem data shifted left by 4
+  9: oUop = { `gadd,  `vmem_addr, `r1, `bgtoffset };// get address for tile memory with r1 and BG tile offset
+  10: oUop = { `grvmem, `gnull, `gnull, `gnull };//read from tile memory stores in vmem data
+  11: oUop = {  `gwrr,   `bh, `vmem_data, `gnull };// save in BH for high byte
+  12: oUop = {  `gaddl,  `vmem_addr, 10'd1  };// get adress for BL adding one to current adress, reads next byte
+  13: oUop = {  `grvmem, `gnull, `gnull, `gnull }; // save in vmem data
+  14: oUop = {  `gwrr,   `bl, `vmem_data, `gnull };//save in BL for low byte
+  15: oUop = {  `gwbg,   `gnull,`gnull, `gnull};// BH and BL go through bg pallete and result gets saved in framebuffer
+  
+//defines loop for getting the same row for the next tile
+//TODO 8191 where does it come from?
+  16: oUop = { `gsubl, `r1, 10'd8191}; 
   17: oUop = { `gsub, `r1, `fbuffer_addr, `r8191};
   18: oUop = { `gjz, 15'h2};
-
-
   19: oUop = {  `gaddl, `cur_tile, 10'd1  };
   20: oUop = {  `gsubl, `r2, 10'd1 };
   21: oUop = {  `gjnz ,  15'd5 };
-
   22: oUop = {  `gaddl,  `tile_row, 10'd2  };
   23: oUop = {  `gaddl , `ly, 10'd1 };
-
-
-
   24: oUop = {  `gwrr,  `r1, `tile_row, `gnull };
   25: oUop = {  `gsubl,  `r1, 10'h10 };
-
   26: oUop = {  `gjz , 15'd3  };
+
+//defines jump for next row of pixels 
   27: oUop = {  `gsubl,  `cur_tile, 10'd32 };
-
-
   28: oUop = {  `ggoto,  15'd4 };
 
   endcase
