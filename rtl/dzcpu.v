11f5d85a0321f26d5982faecc6150f81d0affb5f
diegovalverde <diego.valverde.g@gmail.com>
2016-04-09 13:42:37 -0600

    chg: dev: Implemented anda uop

--- rtl/dzcpu.v
+++ rtl/dzcpu.v
@@ -455,6 +455,23 @@ begin
       rClearIntLatch      = 1'b0;
     end
 
+    `anda:
+    begin
+      oMCUwe              = 1'b0;
+      rRegSelect          = wUop[4:0];
+      rSetMCOAddr         = 1'b0;
+      rRegWe              = 1'b1;
+      rWriteSelect        = `a;
+      rFlagsWe            = 1'b1;
+      rFlags              = {wZ,wN,6'b0};
+      rUopDstRegData      = {8'b0,wA} & {8'b0,wRegData[7:0]};
+      rOverWritePc        = 1'b0;
+      rMcuReadRequest     = 1'b0;
+      rSetiWe             = 1'b0;
+      rSetiVal            = 1'b0;
+      rClearIntLatch      = 1'b0;
+    end
+
     `addx16:
     begin
       oMCUwe              = 1'b0;
@@ -693,7 +710,7 @@ begin
         rSetiVal            = 1'b0;
         rClearIntLatch      = 1'b1;
      end
-  
+
     default:
     begin
       oMCUwe              = 1'b0;
