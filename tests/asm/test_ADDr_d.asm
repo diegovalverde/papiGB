d3d7f1a119a776b4b09fcc31874540fc8930973c
Javier Pacheco <javier.brito29@gmail.com>
2016-04-26 15:37:00 -0600

    chg: dev: change position of PC to beging test ADDr_d

--- tests/asm/test_ADDr_d.asm
+++ tests/asm/test_ADDr_d.asm
@@ -1,8 +1,10 @@
 SECTION "sec", ROM0
 DS $100
+        jp  $200
+DS $100
         ld sp, $FFFE
         ld  a, $ff
         ld  d, $11
         add a, d
-        ;the value expected is D17 Hex-$11 with flags C=1 H=1
+        ;the value expected is D17 Hex-$10 with flags C=1 H=1
         push af
