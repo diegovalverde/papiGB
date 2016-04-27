a4967ef9757431d59f10b90a1328abcda6950bf9
Javier Pacheco <javier.brito29@gmail.com>
2016-04-26 18:11:55 -0600

    chg: dev: Add test INCBC to script

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -62,6 +62,16 @@ else
 	exit 1
 fi
 
+#INCBC
+make clean && make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_INCBC.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL cafe" pgb_cpu.log
+then
+	echo "Test test_INCBC.dump passed"
+else
+	echo "Test test_INCBC.dump failed"
+	exit 1
+fi
 
 
 
