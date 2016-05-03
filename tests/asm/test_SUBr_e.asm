063dc550e0f9d9552fa4e402c9a52d02fc89b96d
Gabalo <gab.jimenez93@gmail.com>
2016-05-03 00:43:45 -0600

    chg:dev: Updated SUBr_e asm test and dump

--- tests/asm/test_SUBr_e.asm
+++ tests/asm/test_SUBr_e.asm
@@ -1,7 +1,10 @@
 SECTION "sec", ROM0
 DS $100
-        ld  a, 23
-        ld 	e, 3
+        jp  $200
+DS $100
+        ld  a, $00 ; 0000 0000
+        ld 	e, $80 ; 1000 0000
         sub a, e
-        ;the value expected is 20
+        ;Expected: A=$80, Flags Z=0, N=1, H=1, C=0
+        ;AF = $8060
         push af
