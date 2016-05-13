a87972ff7d98faf632ef7f3a1831c351e4fb363a
Javier Pacheco <javier.brito29@gmail.com>
2016-05-13 11:43:27 -0600

    chg: dev: Added XORr_b test to script

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -236,6 +236,18 @@ else
 	exit 1
 fi
 
+#XORr_a
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_XORr_b.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 9300" pgb_cpu.log
+then
+	echo "Test test_XORr_b.dump passed"
+else
+	echo "Test test_XORr_b.dump failed"
+	exit 1
+fi
+
 
 make clean  >/dev/null 2>&1
 echo "-I- Running simulation test1 BIOS (be patient...) "
