cc9e745109e669c5b17123efd3ea80a3805a5534
Javier Pacheco <javier.brito29@gmail.com>
2016-05-13 11:38:49 -0600

    chg: dev: Added XORr_a test to script

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -224,6 +224,17 @@ else
 	exit 1
 fi
 
+#XORr_a
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_XORr_a.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 0080" pgb_cpu.log
+then
+	echo "Test test_XORr_a.dump passed"
+else
+	echo "Test test_XORr_a.dump failed"
+	exit 1
+fi
 
 
 make clean  >/dev/null 2>&1
