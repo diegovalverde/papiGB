d5d3a985164b71bb03c5d92e07da4a0e56fe1ec4
Javier Pacheco <javier.brito29@gmail.com>
2016-05-13 00:25:31 -0600

    chg: dev: Added ORr_a test to script

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -139,6 +139,19 @@ else
 	exit 1
 fi
 
+#ORr_a
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_ORr_a.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 8000" pgb_cpu.log
+then
+	echo "Test test_ORr_a.dump passed"
+else
+	echo "Test test_ORr_a.dump failed"
+	exit 1
+fi
+
+
 make clean  >/dev/null 2>&1
 echo "-I- Running simulation test1 BIOS (be patient...) "
 make DUMPTYPE=none SIMFLAGS="-DSTOP_AFTER_FIRST_FRAME -DLCD_SCXY_DISABLED -DENABLE_CPU_LOG -DENABLE_GPU_LOG" >/dev/null 2>&1
