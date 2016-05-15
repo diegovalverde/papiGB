496262c61834e1e20760a2f6b4694908506b44ff
Gabalo <gab.jimenez93@gmail.com>
2016-05-14 19:05:56 -0600

    chg: dev: Added asm test and dump file for INCr_b instruction

--- tests/asm/test_INCr_b.asm
+++ tests/asm/test_INCr_b.asm
@@ -1,8 +1,12 @@
 SECTION "sec", ROM0
 DS $100
-	ld sp, $FFFE
-        ld  b, 0
-        ld  c, 0
+        jp  $200
+DS $100
+				ld sp, $FFFE
+				ld	a, $00
+				or	a, a		; Clears all flags
+				ld  b, $00
+        ld  c, $00
         inc b
-        ;the value expected is 1
-        push bc
+        ;Expected BC = $0100
+        push bc			; Tests BC
