ae70bd9be9c4b9f28f34f4253358763fcba2acd1
diegovalverde <diego.valverde.g@gmail.com>
2016-05-23 05:08:59 -0600

    fix: dev: Fixed ADDr_h and ADDr_l

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -5,6 +5,35 @@
 #
 
 cd sim/
+
+
+
+#ADDr_h
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_ADDr_h.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 2e20" pgb_cpu.log
+then
+	echo "Test test_ADDr_h.dump passed"
+else
+	echo "Test test_ADDr_h.dump failed"
+	exit 1
+fi
+
+
+
+#ADDr_l
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_ADDr_l.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 9020" pgb_cpu.log
+then
+	echo "Test test_ADDr_l.dump passed"
+else
+	echo "Test test_ADDr_l.dump failed"
+	exit 1
+fi
+
 #ADDr_c
 make clean >/dev/null 2>&1
 make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_ADDr_c.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
