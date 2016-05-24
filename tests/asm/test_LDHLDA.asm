3a6a57d7114cf6339b11e5a8fb6f68472345db1d
Javier Pacheco <javier.brito29@gmail.com>
2016-05-24 15:04:46 -0600

    chg: dev: Added ASM and DUMP to test_LDHLDA

---
+++ tests/asm/test_LDHLDA.asm
@@ -0,0 +1,12 @@
+SECTION "sec", ROM0
+DS $100
+        jp  $200
+DS $100
+        ld hl, $FFFD
+        ld a,  $15
+
+
+        ldd [hl],a
+        ldd [hl],a
+        ;the value expected is 4
+        ;push af
