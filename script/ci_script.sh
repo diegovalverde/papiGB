0386ac1d0c9f2adf2c000d212b29c2ef4bfe366a
Javier Pacheco <javier.brito29@gmail.com>
2016-05-24 17:40:49 -0600

    chg: dev: Added LDHLmr_b test to script

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -431,6 +431,18 @@ else
 	exit 1
 fi
 
+#LDHLmr_b
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_LDHLmr_b.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 504f" pgb_cpu.log
+then
+	echo "Test test_LDHLmr_b.dump passed"
+else
+	echo "Test test_LDHLmr_b.dump failed"
+	exit 1
+fi
+
 
 
 
