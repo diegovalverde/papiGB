9bc52b079c2f1a57a6f3894cd365887e2004cfb7
Javier Pacheco <javier.brito29@gmail.com>
2016-05-24 15:07:31 -0600

    chg: dev: Added LDHLDA test to script

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -395,6 +395,17 @@ else
 	exit 1
 fi
 
+#LDHLDA
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_LDHLDA.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 1515" pgb_cpu.log
+then
+	echo "Test test_LDHLDA.dump passed"
+else
+	echo "Test test_LDHLDA.dump failed"
+	exit 1
+fi
 
 
 
