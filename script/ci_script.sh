156312a92af1308d318f24a95560c60c483371d3
Javier Pacheco <javier.brito29@gmail.com>
2016-05-24 23:39:09 -0600

    chg: dev: Added LDrHLm_c test to script

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -563,6 +563,18 @@ else
 	exit 1
 fi
 
+#LDrHLm_c
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_LDrHLm_c.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 0048" pgb_cpu.log
+then
+	echo "Test test_LDrHLm_c.dump passed"
+else
+	echo "Test test_LDrHLm_c.dump failed"
+	exit 1
+fi
+
 
 
 
