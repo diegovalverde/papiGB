76bfbe754ae2eed25db1c2e46dfb861067278ea2
Javier Pacheco <javier.brito29@gmail.com>
2016-05-25 23:23:23 -0600

    fix: dev: change expect valuefor LDrr_bd test

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -732,6 +732,17 @@ else
 	exit 1
 fi
 
+#LDrr_bd
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_LDrr_bd.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 4097" pgb_cpu.log
+then
+	echo "Test test_LDrr_bd.dump passed"
+else
+	echo "Test test_LDrr_bd.dump failed"
+	exit 1
+fi
 
 
 
