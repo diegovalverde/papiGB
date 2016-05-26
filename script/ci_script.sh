80a869d31176052ca83da751b31270be2f0f4f08
Javier Pacheco <javier.brito29@gmail.com>
2016-05-25 22:00:17 -0600

    chg: dev: Added LDrr_ad test to script

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -660,6 +660,20 @@ else
 	exit 1
 fi
 
+#LDrr_ac
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_LDrr_ad.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 1300" pgb_cpu.log
+then
+	echo "Test test_LDrr_ad.dump passed"
+else
+	echo "Test test_LDrr_ad.dump failed"
+	exit 1
+fi
+
+
+
 
 
 
