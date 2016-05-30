60d2bd6bbe29fc0e0b71eb8f196d72055994b2d2
Javier Pacheco <javier.brito29@gmail.com>
2016-05-29 23:46:37 -0600

    chg: dev: Added LDrr_cl test to script

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -866,6 +866,19 @@ else
 	exit 1
 fi
 
+#LDrr_cl
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_LDrr_cl.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 3612" pgb_cpu.log
+then
+	echo "Test test_LDrr_cl.dump passed"
+else
+	echo "Test test_LDrr_cl.dump failed"
+	exit 1
+fi
+
+
 
 make clean  >/dev/null 2>&1
 echo "-I- Running simulation test1 BIOS (be patient...) "
