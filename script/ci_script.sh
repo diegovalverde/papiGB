3c8a8d21aaa4e3084aee6dbea9f1296e83071735
Javier Pacheco <javier.brito29@gmail.com>
2016-05-31 00:07:30 -0600

    chg: dev: Added LDrr_eb test to script

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -1122,6 +1122,17 @@ else
 	exit 1
 fi
 
+#LDrr_eb
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_LDrr_eb.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 3647" pgb_cpu.log
+then
+	echo "Test test_LDrr_eb.dump passed"
+else
+	echo "Test test_LDrr_eb.dump failed"
+	exit 1
+fi
 
 
 
