e5d9219644cce3ce649c931a0fe0d1cc063e1d14
Javier Pacheco <javier.brito29@gmail.com>
2016-05-30 23:53:13 -0600

    chg: dev: Added ADDr_b test to script

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -91,6 +91,22 @@ else
 	exit 1
 fi
 
+#ADDr_b
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_ADDr_b.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 00b0" pgb_cpu.log
+then
+	echo "Test test_ADDr_b.dump passed"
+else
+	echo "Test test_ADDr_b.dump failed"
+	exit 1
+fi
+
+
+
+
+
 #ADDr_h
 make clean >/dev/null 2>&1
 make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_ADDr_h.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
