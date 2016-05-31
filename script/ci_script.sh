be3d80474380e889236e2629e3878c3e1856c1e8
Javier Pacheco <javier.brito29@gmail.com>
2016-05-30 23:59:24 -0600

    chg: dev: Added ANDr_l test to script

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -78,6 +78,17 @@ else
 	exit 1
 fi
 
+#ANDr_l
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_ANDr_l.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 2120" pgb_cpu.log
+then
+	echo "Test test_ANDr_l.dump passed"
+else
+	echo "Test test_ANDr_l.dump failed"
+	exit 1
+fi
 
 
 #ADDn
