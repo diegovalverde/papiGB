33e296d199de4a751ce4dda64af6962aaeaa5769
diegovalverde <diego.valverde.g@gmail.com>
2016-06-05 13:05:49 -0600

    fix: dev: Fixed XORn

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -449,6 +449,19 @@ else
 	exit 1
 fi
 
+#XORn
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_XORan.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 9200" pgb_cpu.log
+then
+	echo "Test test_XORan.dump passed"
+else
+	echo "Test test_XORan.dump failed"
+	exit 1
+fi
+
+
 #XORr_a
 make clean >/dev/null 2>&1
 make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_XORr_a.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
