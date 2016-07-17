d9e780bf114628b4ee30dadc2081a7e70711b2dc
Marco Torres <marco.torres.810@gmail.com>
2016-07-17 13:24:45 -0600

    sound controller channel 2 added.

--- rtl/sound_controller_modules/SoundControllerChannel2.v
+++ rtl/sound_controller_modules/SoundControllerChannel2.v
@@ -18,7 +18,7 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 //
 ////////////////////////////////////////////////////////////////////////////////////
-// Sound module, channel 2. Squate waves with variable timer, frequency and envelope functions.
+// Sound module, channel 2. Squate waves with variable timmer, configurable frequency and envelope functions.
 ////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -26,126 +26,116 @@ module SoundCtrlChannel2 //parameters
   (
     input wire		iClock,			//CPU CLOCK, 4194304Hz
     input wire		iReset,
-    input wire [15:0]	iREG_SOUND2CNT_L,
-    input wire [15:0]	iREG_SOUND2CNT_H,
+    input wire		iOsc64,			//OSC1 clock 64Hz
+	input wire		iOsc256,		//OSC1 clock 256Hz    
+    input wire		iOsc262k,		//OSC2 clock 131072Hz
+
+    input wire [7:0] iNR21,
+    input wire [7:0] iNR22,
+    input wire [7:0] iNR23,
+    input wire [7:0] iNR24,            
+
     output reg [4:0]	oOut 
   );
 
 	reg [5:0] rLength;
+ 	reg [19:0] rLengthCounter;	
 	reg [1:0] rDutyCycle;
-	reg rEvelopeMode;
-	reg [3:0] rInitialValue;
-
-	reg [10:0] rSoundFrequency;
 	reg rTimedMode;
-	reg rSoundReset;
-
+ 	reg rLengthComplete; 	// Channe disable.
 
 	reg rTone;
-	reg [15:0] rFreqCounter;
+	reg [10:0] rSoundFrequency;
+	reg [10:0] rSoundFrequencyCounter;
 
 	reg [3:0] rStep;
 	reg [18:0] rStepTime;
 	reg [18:0] rStepCounter;
-
- 	reg [19:0] rLengthCounter;
- 	reg rLengthComplete; 	//1 IF SOUND IS OFF BY TIMER, 0 IF SOUND IS ON BY TIMER
+	reg [3:0] rInitialValue;
+	reg rEvelopeMode;
 
  	wire [4:0] up_value, down_value;
 
-  	always @(posedge iClock) begin
-		if (iReset || rSoundReset) begin
-			//REGISTER LOAD
+ 	// register load
+	always @(posedge iClock) begin
+		if (iReset || iNR24[7]) begin // Register reload and counters restart.
 
-	  		rLength <= iREG_SOUND2CNT_L[5:0];
-	  		rDutyCycle <= iREG_SOUND2CNT_L[7:6];
-			rStepTime <= (65536*iREG_SOUND2CNT_L[10:8]);
-			rEvelopeMode <= iREG_SOUND2CNT_L[11];
-			rInitialValue <= iREG_SOUND2CNT_L[15:12];
+			rLength 		<= iNR21[5:0];
+			rLengthCounter 	<= 64-iNR21[5:0]; 	// Decrements to zero then load rLength.
+			rLengthComplete <= 0;			// Disables channel when is asserted.
+			rDutyCycle		<= iNR21[7:6];
+			rTimedMode <= iNR24[6];
 
-	  		rSoundFrequency <= iREG_SOUND2CNT_H[10:0];
-			rTimedMode <= iREG_SOUND2CNT_H[14];
-			rSoundReset <= iREG_SOUND2CNT_H[15];						
-					
-			//TONE REGISTERS
-			rFreqCounter <= (16*(2048-iREG_SOUND2CNT_H[10:0]));
-			rTone <= 0;
-		
-			//STEP REGISTERS
-			rStep <= iREG_SOUND2CNT_L[15:12];
-			rStepCounter <= (65536*iREG_SOUND2CNT_L[10:8]);
-		
-			//LENGTH REGISTERS
-	 		rLengthCounter <= (16384*iREG_SOUND2CNT_L[5:0]);
-	 		rLengthComplete <= (iREG_SOUND2CNT_L[5:0]==0); //DISABLE CHANNEL IF LENGTH IS ZERO
+			rStepTime		<= iNR22[2:0];
+			rStepCounter 	<= iNR22[2:0]; // Decrements to zero then load rStepTime.
+			rEvelopeMode	<= iNR22[3];
+			rInitialValue	<= iNR22[7:4];
+			rStep 			<= iNR22[7:4];
+
+			rTone 			<= 0;
+			rSoundFrequency[7:0] <= iNR23[7:0];
+			rSoundFrequency[10:8] <= iNR24[2:0];
+			rSoundFrequencyCounter <= 2048-{iNR24[2:0],iNR23[7:0]}; 
 
 		end
+	end
 
-		else begin
-
-			rEvelopeMode <= iREG_SOUND2CNT_L[11];
-			rInitialValue <= iREG_SOUND2CNT_L[15:12];
-			rSoundReset <= iREG_SOUND2CNT_H[15];
-
-			//TONE GENERATOR
-			if (rFreqCounter == 0 ) begin
-				rTone <= ~rTone;
-				rFreqCounter <=(16*(2048-rSoundFrequency));
-			end
-			else begin
-				rFreqCounter <= rFreqCounter-1;
-			end
-
-			//STEP GENERATOR
-			if (rStepTime != 0) begin
-				if(rStepCounter == 0) begin
-					if(rEvelopeMode) begin //ENVELOPE INCREASES
-						rStep <= ((rStep == 4'hF) ? rStep : rStep+1); //INCREASES ONLY IF STEP IF LOWER THAN TOP VALUE
-					end
-					else begin //ENVELOPE DECREASES
-						rStep <= ((rStep == 4'h0) ? rStep : rStep-1); //DECREASES ONLY IF STEP IF LOWER THAN BOTTOM VALUE
-					end
-					rStepCounter <= rStepTime;
+  	// step gen: generates the output amplitud value. 
+  	always @(posedge iOsc64) begin
+  		if (rStepTime != 0) begin 			// Check if channel is enabled.
+	  		if (rStepCounter ==1 ) begin
+	  			rStepCounter <= rStepTime; 	// Reset counter.
+				if(rEvelopeMode) begin 		// Envelope mode.
+					rStep <= ((rStep == 4'hF) ? rStep : rStep+1); //INCREASES ONLY IF STEP IF LOWER THAN TOP VALUE
 				end
 				else begin
-					rStepCounter <= rStepCounter-1;	
+					rStep <= ((rStep == 4'h0) ? rStep : rStep-1); //DECREASES ONLY IF STEP IF LOWER THAN BOTTOM VALUE
 				end
-			end
-			else begin
-				rLengthComplete = 1'b1;
-			end
-	
-			//LENGTH COUNTER
-			if (rLengthCounter != 0) begin
-				if(~rLengthComplete) begin
-					rLengthCounter <= rLengthCounter-1;
-				end
-			end
-			else begin // rLengthCounter == 0
-				rLengthComplete <= rTimedMode; //0 FOR CONTINUOUS MODE, 1 FOR TIMED.
-//				rStep <= rInitialValue;
-				if (~rLengthComplete) begin	
-					rLengthCounter <= (16384*rLength);
-					rStepCounter <= rStepTime;					
-				end
-			end
-
-			//RE-MAP UNIT
-			if (rLengthComplete) begin
-				oOut[4:0] <= 5'd15;
-			end
-			else begin
-				if (rTone) begin
-					oOut[4:0] <= up_value[4:0];
-				end
-				else begin
-					oOut[4:0] <= down_value[4:0];
-				end
-			end
-
+	  		end
+	  		else begin
+	  			rStepCounter <= rStepCounter-1;
+	  		end  	
 	  	end
 	end
 
+	// tone gen: generates the frecuency of the output.
+	always @(posedge iOsc262k) begin
+		if (rSoundFrequencyCounter ==0) begin
+			rSoundFrequencyCounter <= 2048-rSoundFrequency;
+			rTone <= ~rTone;
+		end
+		else begin
+			rSoundFrequencyCounter <= rSoundFrequencyCounter-1;
+		end
+	end
+
+
+	// timmer: enable or disable channel output.
+	always @(posedge iOsc256) begin
+		if (rLengthCounter == 0) begin
+			rLengthCounter <= 64-rLength;
+			rLengthComplete <= rTimedMode; // Disable channel only if timmer is enabled.
+		end
+		else begin
+			rLengthCounter <= rLengthCounter-1;
+		end
+	end
+
+	//re-map mux
 	assign up_value = 5'd15 + rStep;
 	assign down_value = 5'd15 - rStep;
+
+	always @(posedge iClock) begin
+		if (rLengthComplete) begin
+			oOut[4:0] <= 5'd15;
+		end
+		else begin
+			if (rTone) begin
+				oOut[4:0] <= up_value[4:0];
+			end
+			else begin
+				oOut[4:0] <= down_value[4:0];
+			end
+		end
+	end
 endmodule	
