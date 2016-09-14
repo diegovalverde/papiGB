a2c0f9449b33ab2181030fbd21da1c57344b5907
Heberth Valverde <hgvaldela@gmail.com>
2016-09-14 11:06:11 -0600

    chg: dev: Included test for JPZnn

--- script/ci_script.sh
+++ script/ci_script.sh
@@ -1346,6 +1346,18 @@ else
 	exit 1
 fi
 
+#JPZnn
+make clean >/dev/null 2>&1
+make SIMFLAGS="-DENABLE_CPU_LOG -DLOAD_CARTRIDGE_FROM_FILE -DCARTRIGDE_DUMP_PATH='\"../tests/asm/test_JPZnn.dump\"' -DSKIP_BIOS -DSIMULATION_TIME_OUT=1000" >/dev/null 2>&1
+
+if grep -q "TEST_RET_VAL 00a0" pgb_cpu.log
+then
+	echo "Test test_JPZnn.dump passed"
+else
+	echo "Test test_JPZnn.dump failed"
+	exit 1
+fi
+
 
 
 make clean  >/dev/null 2>&1
