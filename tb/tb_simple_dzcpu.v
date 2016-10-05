c32144c012e091918242689e4614c377ba5dabe8
Heberth Valverde <hgvaldela@gmail.com>
2016-10-05 10:49:50 -0600

    new: dev: Added HALT instruction

--- tb/tb_simple_dzcpu.v
+++ tb/tb_simple_dzcpu.v
@@ -638,6 +638,7 @@ if (InstCount > `START_DUMP_INSN)
       542:  $fwrite(log,"=== JPZnn  === %h \n", uut.DZCPU.iMCUData );
       512: $fwrite(log,"=== LDHLSPn  === %h \n", uut.DZCPU.iMCUData );
       516: $fwrite(log,"=== DISPATCH_INTERRUPT  === %h \n", uut.DZCPU.iMCUData );
+      547: $fwrite(log,"=== HALT  === %h \n", uut.DZCPU.iMCUData );
       default:
           case (uut.DZCPU.iMCUData)
               `LDrr_aa: $fwrite(log,"=== LDrr_aa  === %h \n", uut.DZCPU.iMCUData );
