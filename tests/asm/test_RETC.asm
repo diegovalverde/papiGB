0f74fdcb9b9d3695a5c165ec320bb51f4dd0e8ab
Heberth Valverde <hgvaldela@gmail.com>
2016-11-12 14:29:11 -0600

    fix: dev: Fixed up RETC's test

--- tests/asm/test_RETC.asm
+++ tests/asm/test_RETC.asm
@@ -3,14 +3,14 @@ DS $100
         jp  $200
 DS $100
         ld sp, $FFFE
-        ld hl, end_label
+        ld hl, carry_label
+        push hl
         ld  a, $ff
         add $01
-        jp  c, carry_label
-        jr  end_label
+        ret c
+        jp  end_label
 carry_label:
         ld  a, $71
-        jp  hl
 end_label:
         ;expected $71b0
         push af
